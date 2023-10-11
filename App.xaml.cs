bb4f07f020542b8b6c35530548542574ca4ac792
Phermonteclaro <92066018+Phermonteclaro@users.noreply.github.com>
2023-10-11 14:07:06 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PhoneDial;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace PhoneDial
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnDial(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string userInput = PhoneNum.Text;
+                
+                string phoneNumber = new string(userInput.Where(char.IsDigit).ToArray());
+
+                // Check if the phone number is empty or less than 11 characters
+                if (string.IsNullOrWhiteSpace(phoneNumber) || phoneNumber.Length != 11)
+                {
+                    await DisplayAlert("Invalid input", "Please enter a valid 11-digit phone number", "OK");
+                }
+
+                else
+                {
+                    
+                    await Launcher.OpenAsync($"tel:{phoneNumber}");
+                }
+            }
+            catch (Exception ex)
+            {
+                
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+    }
+}
